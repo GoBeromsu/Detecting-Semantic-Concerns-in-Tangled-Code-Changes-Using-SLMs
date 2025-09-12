a24edd762c00c594a06d24287631896e521caa90
gobeumsu <gobeumsu@gmail.com>
2025-09-12 11:12:17 +0100

    chore: update CUDA version in environment setup script
    - Updated the CUDA version from 12.1.4 to 12.4.0 in `setup_env.sh` to align with the latest compatibility requirements and improvements.

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -30,7 +30,7 @@ echo "Using Git repository root: $REPO_DIR"
 
 module purge
 module load GCCcore/12.3.0
-module load CUDA/12.1.4
+module load CUDA/12.4.0
 module load Anaconda3/2022.05
 module load cuDNN/8.9.2.26-CUDA-12.1.1
 module load CMake/3.26.3-GCCcore-12.3.0
